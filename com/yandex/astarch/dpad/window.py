87986c3b958aa4d41511764d9fbe75276dff25d2
Alexander Starchenko <a-starch@yandex-team.ru>
2020-12-30 11:19:27 +0300

    Added back key

--- com/yandex/astarch/dpad/window.py
+++ com/yandex/astarch/dpad/window.py
@@ -24,6 +24,10 @@ def send_adb_down():
     send_adb_keyevent(20)
 
 
+def send_adb_back():
+    send_adb_keyevent(4)
+
+
 window = Tk()
 window.title("Dpad")
 
@@ -33,11 +37,13 @@ btnUp = Button(window, text="UP", command=send_adb_up)
 btnCenter = Button(window, text="CENTER", command=send_adb_center)
 btnRight = Button(window, text="RIGHT", command=send_adb_right)
 btnDown = Button(window, text="DOWN", command=send_adb_down)
+btnBack = Button(window, text="BACK", command=send_adb_back)
 
 btnLeft.grid(row=2, column=1)
 btnUp.grid(row=1, column=2)
 btnCenter.grid(row=2, column=2)
 btnRight.grid(row=2, column=3)
 btnDown.grid(row=3, column=2)
+btnBack.grid(row=4, column=1)
 
 window.mainloop()
